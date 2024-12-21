47846e227af592da598eda12a043f76445e0ef45
hajimonsur <taiwomonsuru29@gmail.com>
2024-12-21 15:04:47 +0100

    finished my homepage

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-     <h2>
-      hello from monsur
-      </h2> 
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        Hi, I'm Taiwo Monsuru, a passionate full-stack developer based in Lagos. 
+        I build modern, responsive, and user-friendly applications.
+      </p>
+      <p>
+        Check out my projects, learn more about me, or get in touch!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
